0b3615dec183840694f885bbc2be10cfb883dac5
chrismaganga <chrismaganga3@gmail,com>
2023-08-19 21:19:24 +0200

    update

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './index.css'
+
+function App() {
+  
+  return (
+    <div className='text-cyan-500 text-center text-3xl'>
+      <h2 className='text-red-900'>my blog</h2>
+    </div>
+  )
+}
+
+export default App
